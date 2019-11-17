8c5869014bedf079860b1432d8e659570ce9654b
chiqvu1 <chiquynhvu268@gmail.com>
2019-11-17 11:39:52 -0800

    created viewHolder class

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -17,8 +17,11 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
 import android.graphics.Color
+import android.media.Image
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.recyclerview.widget.RecyclerView
@@ -47,6 +50,13 @@ class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
 
 
         holder.textView.text = item.sleepQuality.toString()
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
+        val quality: TextView = itemView.findViewById(R.id.qualitty_string)
+        val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
