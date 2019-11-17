60f9ae45a85bdbc648a1268cc7466d96cca10b0e
chiqvu1 <chiquynhvu268@gmail.com>
2019-11-17 11:16:04 -0800

    create new SleepNight adapter and use binding to associate it with Recycler View

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -61,6 +61,16 @@ class SleepTrackerFragment : Fragment() {
 
         binding.sleepTrackerViewModel = sleepTrackerViewModel
 
+        //create new SleepNightAdapter and use binding to associate it with RecyclerView
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter  = adapter
+
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
+
         binding.setLifecycleOwner(this)
 
         // Add an Observer on the state variable for showing a Snackbar message
