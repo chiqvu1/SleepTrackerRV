fca4f05c82161a8c56d7925cec63cf5de0e57939
chiqvu1 <chiquynhvu268@gmail.com>
2019-12-14 18:30:48 -0800

    corrected mistake in miss match argument

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -97,7 +97,7 @@ class SleepNightAdapter (val clickListener: SleepNightListener) : androidx.recyc
 
     class ViewHolder private constructor(val binding: ListItemSleepNightBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(clickListener: SleepNightListener,item: SleepNight) {
+        fun bind(item: SleepNight, clickListener: SleepNightListener) {
             binding.sleep = item
             binding.clickListener = clickListener
             binding.executePendingBindings()
